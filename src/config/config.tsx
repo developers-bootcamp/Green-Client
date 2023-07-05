b74b5ec828dfc1770832f4cc3577924c317bb5e5
Sara Leah <109041779+sara1386@users.noreply.github.com>
2023-07-05 22:46:12 +0300

    Login page design

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,6 +1,4 @@
-import dotenv from 'dotenv'
 
-dotenv.config();
 
 const BASE_URL= process.env.DASHBOARD_API_BASE_URL;
 
@@ -9,3 +7,13 @@ const GET_ALL_ORDERS_URL = `${BASE_URL}/orders`;
 const LOG_IN = `${BASE_URL}/login/logIn`
 
 export {GET_ALL_ORDERS_URL, LOG_IN}
+
+export const PALLETE = {
+    BLUE: '#6794CF',
+    YELLOW: '#FAE282',
+    RED: '#EE696A',
+    GREEN: '#7ED787',
+    ORANGE: '#EB9F6E',
+    WHITE: '#FFFFFF',
+    GRAY: '#F2F2F2',
+}
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,41 +1,92 @@
-import { useState, useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios, { AxiosError } from 'axios';
-import { string } from 'prop-types';
-import { Interface } from "readline";
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { Button, IconButton, InputAdornment, Paper, TextField } from '@mui/material';
+import Box from '@mui/material/Box';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import { PALLETE } from '../../config/config';
 
-const Login:React.FC = () => {
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
-   const [err, setErr] = useState("");
-   const navigate =useNavigate()
+const Login: React.FC = () => {
+  const [showPassword, setShowPassword] = useState(false);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [err, setErr] = useState("");
+	const navigate = useNavigate()
 
-   const handleSubmit  = async () => {
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const handleSubmit = async () => {
     try {
       await login();
-    }
-    catch (err : any) {
+    } catch (err : any) {
       setErr(err.response.data?.message);
     }
   }
 
-   const login=async()=>{
-    const res =await axios.post("http://localhost:3600/api/auth/login", {email,password},{
+   const login = async() => {
+    const res = await axios.post("http://localhost:3600/api/auth/login", {email,password},{
       withCredentials:true,
     });
     localStorage.setItem("token",res.data.accesToken)
     navigate("/landingPage ") 
   }
-  
+
   return (
-    <div className="login-page" style={{ paddingTop: "50px" }}>
-      <h1>Login</h1>
-      <input onChange={(e) => { setEmail(e.target.value) }} type="text" placeholder="email" ></input>
-      <input onChange={(e) => { setPassword(e.target.value) }} type="text" placeholder="password"></input>
-      <button onClick={handleSubmit }>login</button> 
-            {err && <p>{err}</p>}
-    </div>
+		<Box sx={{ display: 'flex', flexWrap: 'wrap', width: '100%', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
+			<Paper sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '600px', height: '400px' }}>
+			<p style={{ marginTop: '0px' }}>Log in to your account</p>
+			<h4>Enter your email and password</h4>
+				<div style={{ marginBottom: '10px' }}>
+					<TextField id="email" label="Email address" variant="outlined" sx={{ marginBottom: '10px', width: '300px' }}
+							onChange={(e) => { setEmail(e.target.value) }} />
+				</div>
+				<div>
+					<FormControl sx={{ marginBottom: '10px', width: '300px' }} variant="outlined">
+						<InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+						<OutlinedInput
+							id="outlined-adornment-password"
+							type={showPassword ? 'text' : 'password'}
+							onChange={(e) => { setPassword(e.target.value) }}
+							endAdornment={
+								<InputAdornment position="end">
+									<IconButton
+										aria-label="toggle password visibility"
+										onClick={handleClickShowPassword}
+										onMouseDown={handleMouseDownPassword}
+										edge="end"
+									>
+										{showPassword ? <VisibilityOff /> : <Visibility />}
+									</IconButton>
+								</InputAdornment>
+							}
+							label="Password"
+						/>
+						<Button
+							type='submit'
+							variant="contained"
+							sx={{backgroundColor:PALLETE.YELLOW,
+										bottom: '-30px',
+										left: '50%',
+										transform: 'translateX(-50%)',
+										width: '65%',
+										borderRadius: '15px'}}
+							onClick={handleSubmit}>
+						Sign Up
+					</Button>
+					{err && <p>{err}</p>}
+					</FormControl>
+				</div>
+				
+			</Paper>
+		</Box>
   );
-}
+};
 
 export default Login;
