d170709ed3dc1d8eb6524c3f6e7e1a722f717f12
ShaniWell <136933202+ShaniWell@users.noreply.github.com>
2023-07-20 15:02:05 +0300

    update

--- src/pages/landingPage/TabsComponent.styles.tsx
+++ src/pages/landingPage/TabsComponent.styles.tsx
@@ -1,30 +1,15 @@
  import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+ import { PALLETE } from '../../config/config';
+ import { styled } from '@mui/system';
 
 export const useStyles = makeStyles({
     Tab:{
         textTransform: "none",
         fontSize:"125%",
-        color: "black"
+        color: PALLETE.WHITE
     }
  })
-
-//  import { styled } from '@mui/system';
-// import { PALLETE } from '../../config/config';
-
+ //example how to use with styled
 // export const MyTxtField = styled('Tab')({
 //     width: '90% !important'
-// })
-
-// export const MyMsdError = styled('div')({
-//     color: `${PALLETE.RED} !important`,
-//     fontSize: "10px"
-// })
-
-// export const MyArrowIcon = styled('div')({
-//     color: 'red !important',
-// })
-
-// export const MyFieldContainer = styled('div')({
-//     display: 'flex',
-//     alignItems: 'center',
 // })
