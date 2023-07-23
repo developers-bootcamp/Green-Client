cee64fd6a54b59ecc1ccd0ae7b5935b788139a0f
fraidy1 <136713067+fraidy1@users.noreply.github.com>
2023-07-23 09:16:43 +0300

    try

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,7 @@ import { Reducer, createStore } from 'redux';
 import Loader from './components/globalLoader/Loader';
 
 function App() {
-  // const [isLoading, setIsLoading] = useState(false);
 
-  // const startLoading = () => {
-  //   setIsLoading(true);
-  // };
-
-  // const stopLoading = () => {
-  //   setIsLoading(false);
-  // };
 
   const rootReducer: Reducer<any, any> = (state = {}, action) => {
     switch (action.type) {
