d1228808bf7c49ddc47c38a7fbbd655acab6423c
RivkaEhrenfeld <hr058325@gmail.com>
2023-07-26 12:28:51 +0300

    ss

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        {<Loader />}
+        {/* {<Loader />} */}
         <Routes>
           <Route path="/" element={<TabsComponent/>} />
           <Route path="/landingPage" element={<LandingPage/>} />
--- src/components/globalLoader/Loader.tsx
+++ src/components/globalLoader/Loader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/redux/store';
 import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
+//import CircularProgress from '@material-ui/core/CircularProgress';
 
 const Loader: React.FC = () => {
   const isLoading = useSelector((state:RootState) => state.app?.isLoading || false);
@@ -17,7 +17,7 @@ const Loader: React.FC = () => {
            sx={{ color: '#00000080;', zIndex: (theme) => theme.zIndex.drawer + 1 }}
           open={isLoading}
            >
-           <CircularProgress color="primary" />
+           {/* <CircularProgress color="primary" /> */}
          </Backdrop>
        </div>
 }
