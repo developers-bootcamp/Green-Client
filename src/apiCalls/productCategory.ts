d2d4fcf2a0b962cd6d9ff7423685e05dde58ed1d
avigailRo <avigail4192@gmail.com>
2023-09-03 19:30:51 +0300

    before chani

--- src/apiCalls/productCategory.ts
+++ src/apiCalls/productCategory.ts
@@ -2,35 +2,14 @@ import axios from "axios"
 import { GET_ALL_PRODUCT_CATEGORY ,DELETE_PRODUCT_CATEGORY,EDIT_PRODUCT_CATEGORY, ADD_PRODUCT_CATEGORY} from "../config/config"
 import { IProductCategory } from "../interfaces/model/IProductCategory";
 export const getAllCategory=async()=>{
-    let t=localStorage.getItem("token");
-    if(t==undefined)
-        t="qqq"
-    const config = { headers: { 'Authorization': t} };
-return await axios.get(`${GET_ALL_PRODUCT_CATEGORY}`,config);
+return await axios.get(`${GET_ALL_PRODUCT_CATEGORY}`);
 } 
 export const deleteproductCategory=async(id:string)=>{
-    let t=localStorage.getItem("token");
-    if(t==undefined)
-        t="qqq"
-        
-    const config = { headers: { 'Authorization': t} };
-return await axios.delete(`${DELETE_PRODUCT_CATEGORY}/${id}`,config);
+return await axios.delete(`${DELETE_PRODUCT_CATEGORY}/${id}`);
 } 
 export const editProductCategory = async (id:any,productCategory:IProductCategory) => {
-    let t = localStorage.getItem("token");
-    if (t == undefined)
-        t = "qqq"
-        
-    const config = {headers: { 'Authorization': t } };
-
-    return await axios.put(`${EDIT_PRODUCT_CATEGORY}/${id}`,productCategory, config);
+    return await axios.put(`${EDIT_PRODUCT_CATEGORY}/${id}`,productCategory);
 } 
 export const addProductCategory = async (productCategory:IProductCategory) => {
-    let t = localStorage.getItem("token");
-    if (t == undefined)
-        t = "qqq"
-        
-    const config = {headers: { 'Authorization': t } };
-
-    return await axios.post(`${ADD_PRODUCT_CATEGORY}`,productCategory, config);
+    return await axios.post(`${ADD_PRODUCT_CATEGORY}`,productCategory);
 } 
