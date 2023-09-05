78746774cbc6bd0c47f66b4574cf79f60938f56e
zipora-zaide <bsdzz827@gmail.com>
2023-09-05 13:05:26 +0300

    change

--- src/pages/landingPage/tabComponnent/dashboard/BarChart.tsx
+++ src/pages/landingPage/tabComponnent/dashboard/BarChart.tsx
@@ -15,107 +15,30 @@ export const options = {
 
 const BarChart:React.FC = () => {
       
-  const [BarChartData, setBarChartData] = useState([
-    {
-      "month": 1,
-      "year": 2023,
-      "products": [
-        {
-          "product": "Product 1",
-          "quantity": 20
-        },
-        {
-          "product": "Product 2",
-          "quantity": 40
-        },
-        {
-          "product": "Product 3",
-          "quantity": 50
-        },
-        {
-          "product": "Product 4",
-          "quantity": 60
-        },
-        {
-          "product": "Product 5",
-          "quantity": 30
-        }
-      ]
-    },
-    {
-      "month": 2,
-      "year": 2023,
-      "products": [
-        {
-          "product": "Product 1",
-          "quantity": 10
-        },
-        {
-          "product": "Product 2",
-          "quantity": 20
-        },
-        {
-          "product": "Product 3",
-          "quantity": 30
-        },
-        {
-          "product": "Product 4",
-          "quantity": 40
-        },
-        {
-          "product": "Product 5",
-          "quantity": 80
-        }
-      ]
-    },
-    {
-      "month": 3,
-      "year": 2023,
-      "products": [
-        {
-          "product": "Product 1",
-          "quantity": 40
-        },
-        {
-          "product": "Product 2",
-          "quantity": 70
-        },
-        {
-          "product": "Product 3",
-          "quantity": 10
-        },
-        {
-          "product": "Product 4",
-          "quantity": 15
-        },
-        {
-          "product": "Product 5",
-          "quantity": 10
-        }
-      ]
-    },
-  ]);
+  const [BarChartData, setBarChartData] = useState([]);
+    
+  
 
-  // useEffect(() => {
-  //   topProduct().then(res => {
-  //         setBarChartData(res.data)
-  //     }).catch(err => {
-  //       console.error(err)
-  //     })
-  //   }, []);
+  useEffect(() => {
+    topProduct().then(res => {
+          setBarChartData(res.data)
+      }).catch(err => {
+        console.error(err)
+      })
+    }, []);
 
  
-    const products =  BarChartData.map((monthlyProductSalesResult) => {
-      return monthlyProductSalesResult.products.map((productData) => {
+    const products =  BarChartData.map((monthlyProductSalesResult:any) => {
+      return monthlyProductSalesResult.products.map((productData:any) => {
         return productData.product;
       });
     }).slice(-1).flat();
 
-    const monthAndQuantity =BarChartData.map((monthlyProductSalesResult) => {
+    const monthAndQuantity =BarChartData.map((monthlyProductSalesResult:any) => {
       return [
        monthlyProductSalesResult.month > 10 ? monthlyProductSalesResult.month + "/" + String(monthlyProductSalesResult.year).slice(2):
         "0" + monthlyProductSalesResult.month + "/" + String(monthlyProductSalesResult.year).slice(2),
-        ...monthlyProductSalesResult.products.map((productData) => productData.quantity),
+        ...monthlyProductSalesResult.products.map((productData:any) => productData.quantity),
       ];
     });
 
